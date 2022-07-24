95e8a4c806c1c764c472e0539362e0f089ac140b
Marina Sereda <marina.gorb@gmail.com>
2022-07-24 15:05:00 +0300

    Replace hardcoded numbers with variables

--- src/modal-menu.js
+++ src/modal-menu.js
@@ -1,29 +1,39 @@
 (() => {
   // === For Header animation ===
   const headerSection = document.querySelector('.header');
-  let currentPosition;
+  const heroSection = document.querySelector('.hero');
 
-  window.addEventListener('scroll', function () {
-    currentPosition = scrollY;
-    console.log(currentPosition);
-    if (currentPosition < 100) {
+  window.addEventListener('scroll', scrollHandler);
+
+  function scrollHandler() {
+    const currentPosition = scrollY;
+    const headerSectionHeight = headerSection.offsetHeight;
+    const heroSectionHeight = heroSection.offsetHeight;
+
+    if (currentPosition < headerSectionHeight) {
       headerSection.classList.remove('is-fixed');
       headerSection.classList.remove('show');
       headerSection.classList.remove('hide');
-    } else if (currentPosition >= 100 && currentPosition < 300) {
+    } else if (
+      currentPosition >= headerSectionHeight &&
+      currentPosition < heroSectionHeight - headerSectionHeight
+    ) {
       headerSection.classList.add('is-fixed');
       headerSection.classList.remove('show');
       headerSection.classList.remove('hide');
-    } else if (currentPosition >= 300 && currentPosition < 500) {
+    } else if (
+      currentPosition >= heroSectionHeight - headerSectionHeight &&
+      currentPosition < heroSectionHeight
+    ) {
       headerSection.classList.add('is-fixed');
       headerSection.classList.remove('show');
       headerSection.classList.add('hide');
-    } else if (currentPosition >= 500) {
+    } else if (currentPosition >= heroSectionHeight) {
       headerSection.classList.add('is-fixed');
       headerSection.classList.add('show');
       headerSection.classList.remove('hide');
     }
-  });
+  }
 
   // === For Menu start ===
   const menuBtn = document.querySelector('[data-menu-button]');
