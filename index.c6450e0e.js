8a74c58ee5c7fed1c6b5c3d0685975a33bd11fb7
svitlanapl <105806771+svitlanapl@users.noreply.github.com>
2022-07-24 19:51:35 +0000

    Deploying to gh-pages from @ svitlanapl/command-project@fa3c99d95dcf59af7923cdb4e22577d5f08569a8 🚀

---
+++ index.c6450e0e.js
@@ -0,0 +1,2 @@
+function myFunction(){var e=document.getElementById("dots"),n=document.getElementById("more"),t=document.getElementById("myBtn");"none"===e.style.display?(e.style.display="inline",t.innerHTML="Read More",n.style.display="none"):(e.style.display="none",t.innerHTML="Read Less",n.style.display="inline")}
+//# sourceMappingURL=index.c6450e0e.js.map
--- index.ef4a4aca.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=index.ef4a4aca.js.map
