003e016eb18d41a5e8c2416a544c464fc894483a
Marina Sereda <marina.gorb@gmail.com>
2022-07-24 14:41:09 +0300

    Add animation to header
    Доки на екрані є hero, header - прозорий і не фіксований. Коли зникає з екрана hero, з'являється фіксований header.

--- src/modal-menu.js
+++ src/modal-menu.js
@@ -1,4 +1,30 @@
 (() => {
+  // === For Header animation ===
+  const headerSection = document.querySelector('.header');
+  let currentPosition;
+
+  window.addEventListener('scroll', function () {
+    currentPosition = scrollY;
+    console.log(currentPosition);
+    if (currentPosition < 100) {
+      headerSection.classList.remove('is-fixed');
+      headerSection.classList.remove('show');
+      headerSection.classList.remove('hide');
+    } else if (currentPosition >= 100 && currentPosition < 300) {
+      headerSection.classList.add('is-fixed');
+      headerSection.classList.remove('show');
+      headerSection.classList.remove('hide');
+    } else if (currentPosition >= 300 && currentPosition < 500) {
+      headerSection.classList.add('is-fixed');
+      headerSection.classList.remove('show');
+      headerSection.classList.add('hide');
+    } else if (currentPosition >= 500) {
+      headerSection.classList.add('is-fixed');
+      headerSection.classList.add('show');
+      headerSection.classList.remove('hide');
+    }
+  });
+
   // === For Menu start ===
   const menuBtn = document.querySelector('[data-menu-button]');
   const mobileMenu = document.querySelector('[data-menu]');
